b7facb766faae72ffa456d47bcfdd01685785651
yy <3082210897@qq.com>
2026-05-08 14:11:02 +0800

    feat: set application icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "translate.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icons/assets/app-icon-1024.png"));
     Translate w;
     w.show();
     return a.exec();
